8309f40a428b64c58c62bd467a7241ce6fc5e6c7
Nikhil Chatterjee <nikhilc1527@gmail.com>
2026-01-14 15:08:48 -0800

    updated scripts

--- .scripts/fake_hdmi.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DisplayPort-0 --off --output HDMI-A-0 --mode 1920x1200 --pos 960x1200 --rotate normal --output DVI-D-0 --off --output DVI-D-1 --off --output DisplayPort-1 --mode 1920x1200 --pos 0x0 --rotate normal --output DisplayPort-2 --mode 1920x1200 --pos 1920x0 --rotate normal
--- .scripts/get_news.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# requires jq
-
-# Associated Press
-cat /tmp/news.txt | jq '.["hub"] | .["data"] | .["/us-news"].cards | .[] | .contents | .[] | .headline, .firstWords, .storyHTML'
